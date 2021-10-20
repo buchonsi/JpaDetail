da82bed5dde849e4350074db214a3c65548db8d5
buchoni <buchonsi@naver.com>
2021-10-20 09:30:58 +0900

    item/item Repo 추가

--- src/main/java/jpastudy/jpashop/domain/item/Item.java
+++ src/main/java/jpastudy/jpashop/domain/item/Item.java
@@ -1,5 +1,6 @@
 package jpastudy.jpashop.domain.item;
 
+import jpastudy.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,4 +24,17 @@ public abstract class Item {
 
     //재고수량
     private int stockQuantity;
+
+    //==비즈니스 로직==//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
