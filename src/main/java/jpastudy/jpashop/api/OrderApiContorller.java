768ba0b0241ac6aa601621938e0938495b0ea744
buchoni <buchonsi@naver.com>
2021-10-21 17:57:27 +0900

    성능튜닝

--- src/main/java/jpastudy/jpashop/api/OrderApiContorller.java
+++ src/main/java/jpastudy/jpashop/api/OrderApiContorller.java
@@ -1,14 +1,16 @@
 package jpastudy.jpashop.api;
 
-import jpastudy.jpashop.domain.Order;
-import jpastudy.jpashop.domain.OrderItem;
-import jpastudy.jpashop.domain.OrderSearch;
+import jpastudy.jpashop.domain.*;
 import jpastudy.jpashop.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 /* OneToMany 관계 성능 최적화
  * Order -> OrderItem -> Item
@@ -40,6 +42,62 @@ public class OrderApiContorller {
     /**
      * V2 : 엔티티를 DTO로 변환해서 노출하는 방식
      * N+1문제 발생
+     * 엔티티를 직접 노출하지 않았다는 장점이 있음(v1과 비교했을때)
      */
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2(){
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+        return orders.stream()                          //Stream<Order>
+                .map(order -> new OrderDto(order))      //Stream<OrderDto>
+                .collect(toList());                     //List<OrderDto>
+    }
 
+    /**
+     * V3 : 엔티티를 DTO로 변환하여 노출, Fetch join을 사용하여 성능 최적화
+     * 문제점 : xxxToMany 의존관계 객체들의 페이징 처리가 안된다.
+     */
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(toList());
+        return result;
+    }
+
+    //응답과 요청에 사용항 DTO InnerClass 선언
+
+    @Data
+    static class OrderItemDto {
+        private String itemName; //상품 명
+        private int orderPrice; //주문 가격
+        private int count; //주문 수량
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    } //static class OrderItemDto
+
+    @Data
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+
+            orderItems = order.getOrderItems()
+                    .stream()                                         //Stream<OrderItem>
+                    .map(orderItem -> new OrderItemDto(orderItem))    //Stream<OrderItemDTO>
+                    .collect(toList());                    //List<OrderItemDTO>
+        }
+    } //static class OrderDto
 }
